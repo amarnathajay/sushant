249edbbc777dd80e92190c038d88af4dfcb0473d
Ajay Amarnath <ajay@moonraft.com>
2015-03-30 14:43:43 +0530

    First Commit

--- js/silly.js
+++ js/silly.js
@@ -1,13 +1,13 @@
 var SILLY = (function(module){
 
 	module.DoIt = function(resultObject){
-		if (typeof navigator !== "undefined" && navigator.app) {
-        // Mobile device.
+		// if (typeof navigator !== "undefined" && navigator.app) {
+  //       // Mobile device.
         navigator.app.loadUrl('http://www.google.com/', {openExternal: true});
-    } else {
-        // Possible web browser
-        window.open("http://www.google.com/", "_blank");
-    }
+    // } else {
+    //     // Possible web browser
+    //     window.open("http://www.google.com/", "_blank");
+    // }
 
 		// window.location.href = "http://www.google.com";
 		// resultObject.prepend(Date() + '<br/>');
